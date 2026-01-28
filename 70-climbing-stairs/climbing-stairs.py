b49d24d55b77b2044ef05884a857dbf960b9098b
ARCHISMAN DAS <dasarchisman21@gmail.com>
2026-01-28 15:45:41 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (21.73%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,6 +1,9 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        count_map = [-1]*46
+        if n==1 or n==2 or n==3:
+            return n
+        count_map = [-1] * (n+1)
+        count_map[0] = 0
         count_map[1] = 1
         count_map[2] = 2
         for i in range(3, n+1):
