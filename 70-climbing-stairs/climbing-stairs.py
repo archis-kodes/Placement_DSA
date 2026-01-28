67625531660a3aa2eaa5df6d56b78b8f4aefd0b8
ARCHISMAN DAS <dasarchisman21@gmail.com>
2026-01-28 15:38:38 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (21.73%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,15 +1,8 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        count_map = dict()
-        def traverse(current_stair, target):
-            if current_stair>target:
-                return 0
-            if current_stair in count_map:
-                return count_map[current_stair]
-            if current_stair==target:
-                return 1
-            count_map[current_stair+1] = traverse(current_stair+1, target)
-            count_map[current_stair+2] = traverse(current_stair+2, target)
-            return count_map[current_stair+1]+count_map[current_stair+2]
-        print(count_map)
-        return traverse(0, n)
+        count_map = [-1]*46
+        count_map[1] = 1
+        count_map[2] = 2
+        for i in range(3, n+1):
+            count_map[i] = count_map[i-1] + count_map[i-2]
+        return count_map[n]
