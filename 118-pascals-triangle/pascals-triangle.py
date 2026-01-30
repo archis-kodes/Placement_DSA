394286621448b44e0a6eac3042e558568b13418b
ARCHISMAN DAS <dasarchisman21@gmail.com>
2026-01-31 01:01:34 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (13.67%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.py
@@ -0,0 +1,10 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        res = [[1]]
+        for i in range(numRows-1):
+            temp= [1]
+            for j in range(len(res[i])-1):
+                temp.append(res[i][j] + res[i][j+1])
+            temp.append(1)
+            res.append(temp)
+        return res
