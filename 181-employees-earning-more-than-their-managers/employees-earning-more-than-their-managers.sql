33e523aa4c47ca691f9e39de52c7bffea6c8e79d
ARCHISMAN DAS <dasarchisman21@gmail.com>
2026-02-06 00:30:57 +0530

    Time: 346 ms (80.51%) | Memory: 0B (100.00%) - LeetSync

---
+++ 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+select e2.name
+as Employee
+from Employee e1
+inner join Employee e2
+on e1.id = e2.managerID
+where e1.salary < e2.salary
+
+# e1 --> Manager
+# e2 --> Employee
