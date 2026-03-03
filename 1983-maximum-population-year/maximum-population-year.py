98f2ead8d403878dff34ea12f84586526500601e
ARCHISMAN DAS <dasarchisman21@gmail.com>
2026-03-03 19:22:11 +0530

    Time: 4 ms (17.96%) | Memory: 19.5 MB (25.55%) - LeetSync

--- 1983-maximum-population-year/maximum-population-year.py
+++ 1983-maximum-population-year/maximum-population-year.py
@@ -1,18 +1,19 @@
 class Solution:
     def maximumPopulation(self, logs: List[List[int]]) -> int:
-        L = []
-        for log in logs:
-            L.append([log[0], 1])
-            L.append([log[1], -1])
-        L.sort()
-        print(L)
-        # Find Max
-        max_year = L[1][0]
-        maximum = 0
-        add = 0
-        for i in L:
-            add += i[1]
-            if add>maximum:
-                maximum = add
-                max_year = i[0]
+        n = len(logs)
+        for i in range(n):
+            logs[len(logs):len(logs)] = ([logs[i][0], 1], [logs[i][1], -1])
+        logs[0:n] = []
+        print(logs)
+        
+        logs.sort()
+
+        max_population = 0
+        max_year = logs[0][0]
+        population = 0
+        for i in range(len(logs)):
+            population += logs[i][1]
+            if population > max_population:
+                max_population = population
+                max_year = logs[i][0]
         return max_year
