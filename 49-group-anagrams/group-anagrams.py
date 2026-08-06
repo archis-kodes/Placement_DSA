0d6261189744260fa584fa0b655145b621defffa
ARCHISMAN DAS <dasarchisman21@gmail.com>
2026-08-06 23:06:07 +0530

    Time: 31 ms (7.45%) | Memory: 21.8 MB (87.96%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,8 +1,20 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        def generate(words):
+            array = [0]*26
+            result = []
+            # Frequency Maintain
+            for i in words:
+                array[ord(i) - ord('a')] += 1
+            # Join
+            for i in range(26):
+                if array[i]!=0:
+                    result.append(chr(97+i)*array[i])
+            return "".join(result)
+
         memory = dict()
         for i in strs:
-            sorted_i = "".join(sorted(i))
+            sorted_i = generate(i)
             if sorted_i not in memory:
                 memory[sorted_i] = [i]
             else:
